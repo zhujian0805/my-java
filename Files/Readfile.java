2dfb48c3f0b4d8553ca2b2ed9269f52126912601
James Zhu <zhujian0805@gmail.com>
2015-08-02 21:34:01 +0800

    mod

---
+++ Files/Readfile.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Readfile {
+  public static void main(String[] args)  {
+    try {
+        BufferedReader in = new BufferedReader (new FileReader("/etc/passwd"));
+        String str;
+        while ((str = in.readLine()) != null) {
+          System.out.println(str);
+        }
+      }
+    catch (IOException e) {
+    }
+  }
+}
